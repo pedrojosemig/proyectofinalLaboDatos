78c5fb88b9c438d65dcd87856c52a2136548bfb1
pedrojosemig <98707722+pedrojosemig@users.noreply.github.com>
2024-12-05 22:31:02 +0100

    cambios

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
 
+# aplicación Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "¡Hola desde la API Flask en Heroku!"
+# cargo el modelo guardado
+model = joblib.load('random_forest_model.joblib')
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Heroku asigna un puerto dinámico
-    app.run(host='0.0.0.0', port=port)
+# endpoint para realizar predicciones
+@app.route('/predict', methods=['POST'])
+def predict():
+    # obtengo los datos de la solicitud
+    data = request.get_json()  # JSON con los datos de entrada
+    features = np.array(data['features']).reshape(1, -1)
+    
+    # prediccion
+    prediction = model.predict(features)
+    
+    # prediccin como respuesta
+    return jsonify({'prediction': prediction.tolist()})
 
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-
-# Ejemplo de una ruta
-@app.route('/')
-def hello_world():
-    return "¡Hola desde la API Flask en Heroku!"
-
-# Aquí puedes agregar otras rutas que tengas en tu Jupyter Notebook
-# Por ejemplo:
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     data = request.get_json()
-#     # Aquí va tu código para hacer predicciones con el modelo
-#     return jsonify(result="Aquí va el resultado de tu predicción")
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Heroku asigna un puerto dinámico
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
