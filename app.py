c3e19c3f58c8fdf4bd89785f5a959cbd7ee45f0a
pedrojosemig <98707722+pedrojosemig@users.noreply.github.com>
2024-12-05 22:35:34 +0100

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os  # Importar os para acceder a variables de entorno
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -16,11 +16,12 @@ def predict():
     data = request.get_json()  # JSON con los datos de entrada
     features = np.array(data['features']).reshape(1, -1)
     
-    # prediccion
+    # predicción
     prediction = model.predict(features)
     
-    # prediccin como respuesta
+    # predicción como respuesta
     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Heroku asigna un puerto dinámico
+    app.run(host='0.0.0.0', port=port, debug=True)  # Asegúrate de pasar el puerto a app.run()
