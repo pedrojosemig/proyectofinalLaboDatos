c788701958bfda1532109d503b3bb7fa5e5ae02f
pedrojosemig <98707722+pedrojosemig@users.noreply.github.com>
2024-12-08 17:26:02 +0100

    updates

--- notebook_final.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# cargo el insumo
-dataset_path = r"C:\Users\DELL\Documents\GitHub\proyectofinalLaboDatos\synthetic_customer_data.csv"
-data = pd.read_csv(dataset_path)
-
-
-print(data.head())
-print(data.info())
